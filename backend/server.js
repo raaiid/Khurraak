ac5b4df61736a5c95cce4ad801b467a4e032942d
Abid Gul Shahid <silencemind55@gmail.com>
2021-09-21 23:26:27 +0500

    image not showing problem solved
    image not showing problem solved

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ const cors = require('cors')
 const app = express()
 const path = require('path')
 app.use(express.json())
+var publicDir = require('path').join(__dirname,'/public/static');
+app.use(express.static(publicDir));
+
 
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname+'/design2.html'));
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 
 })
 
